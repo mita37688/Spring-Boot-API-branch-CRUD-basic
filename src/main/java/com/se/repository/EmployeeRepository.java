7cfa6b1910af815041f3d5603e47861a37584a34
MSI\Minh Tan <tanlun04112000@gmail.com>
2021-09-15 22:31:14 +0700

    entity, service, repo, controller

--- src/main/java/com/se/repository/EmployeeRepository.java
+++ src/main/java/com/se/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.se.repository;
 
 import com.se.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
